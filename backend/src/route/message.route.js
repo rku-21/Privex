7f4a46a62ab4e8fd38134fc82478805fcca5233e
rku-21 <rahul.k.upadhyay21@gmail.com>
2025-09-01 20:30:01 +0530

    completed

--- backend/src/route/message.route.js
+++ backend/src/route/message.route.js
@@ -6,11 +6,11 @@ router.get("/friends-requests", protectRoute, getAllrequests);
 router.get("/friends-send",protectRoute,getAllsendRequest);
 router.get("/users",protectRoute, getAllUsers);
 router.get("/friends", protectRoute, getAllfriends);
-router.get("/:id", protectRoute, getMessagesBetweenUsers);
 router.post("/send/:id", protectRoute,sendMessges);
 router.post("/friends/send/:id",protectRoute,friendRequestSend);
 router.delete("/friends/remove/request/:id", protectRoute,friendRejected);
 router.post("/friends/accept/:id",protectRoute,friendRequestAccept);
+router.get("/:id", protectRoute, getMessagesBetweenUsers);
 
 
 
