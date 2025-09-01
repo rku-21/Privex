9202ec49f3f46734402516eaeaf9befd5da3d3ff
rku-21 <rahul.k.upadhyay21@gmail.com>
2025-09-01 20:13:58 +0530

    project Completed !

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,7 +28,7 @@ if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname,"../frontend/chat_app/dist")));
 
 
-  app.get("*",(req,res)=>{
+  app.get("/*",(req,res)=>{
     res.sendFile(path.join(__dirname,"../frontend/chat_app","dist","index.html"));
   })
 }
